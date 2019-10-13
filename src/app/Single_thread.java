65b3e13869f7800735ad14b1f2ab73706893f471
oden007 <49471503+oden007@users.noreply.github.com>
2019-10-13 18:33:38 +0900

    Update Single_thread.java

--- src/app/Single_thread.java
+++ src/app/Single_thread.java
@@ -14,6 +14,7 @@ class Blockchain{
     int cash_data;//トランザクションデータ
     String tarnsaction;
     String hash;//自身のハッシュ値
+    int difficulty=;
     //difficultyがマイニングの難易度に直結するのでマイニング時間に応じて変更させる(デフォルトは6)
     public Blockchain(String previous_hash,int cash_data,String transaction){
         this.timestamp=new Date();
